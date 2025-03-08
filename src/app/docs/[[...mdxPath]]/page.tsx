0de5566cb4cfbc8bc71e9e0e072d4265301c07ef
process_black <process_black@mac.lan>
2025-03-08 18:53:33 -0500

    Updates:

--- src/app/docs/[[...mdxPath]]/page.tsx
+++ src/app/docs/[[...mdxPath]]/page.tsx
@@ -3,11 +3,12 @@ import { useMDXComponents as getMDXComponents } from '@/mdx-components'
  
 export const generateStaticParams = generateStaticParamsFor('mdxPath')
  
-export async function generateMetadata(props) {
-  const params = await props.params
-  const { metadata } = await importPage(params.mdxPath)
-  return metadata
+export async function generateMetadata(props: { params: { mdxPath: string[] } }) {
+  const { params } = props;
+  const { metadata } = await importPage(params.mdxPath);
+  return metadata;
 }
+
  
 const Wrapper = getMDXComponents().wrapper
  
--- src/app/posts/[[...mdxPath]]/page.tsx
+++ src/app/posts/[[...mdxPath]]/page.tsx
@@ -3,10 +3,10 @@ import { useMDXComponents as getMDXComponents } from '@/mdx-components'
  
 export const generateStaticParams = generateStaticParamsFor('mdxPath')
  
-export async function generateMetadata(props) {
-  const params = await props.params
-  const { metadata } = await importPage(params.mdxPath)
-  return metadata
+export async function generateMetadata(props: { params: { mdxPath: string[] } }) {
+  const { params } = props;
+  const { metadata } = await importPage(params.mdxPath);
+  return metadata;
 }
  
 const Wrapper = getMDXComponents().wrapper
